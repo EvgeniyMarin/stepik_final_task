a2a98e14cb658d116b2166f5e5df9cf22ac89439
EvgeniyMarin <evgeniymarin7@mail.ru>
2023-10-21 16:08:58 +0300

    добавление файлов финального проекта stepik

---
+++ conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: en, ru ...")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    driver_browser = webdriver.Chrome(options=options)
+    driver_browser.get(link)
+    yield driver_browser
+    driver_browser.quit()
