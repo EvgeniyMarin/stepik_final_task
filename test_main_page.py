7adb6f2dee33852c9000568be035fbff3988a009
EvgeniyMarin <evgeniymarin7@mail.ru>
2023-11-22 11:45:22 +0300

    improving code readability

--- test_main_page.py
+++ test_main_page.py
@@ -1,29 +1,52 @@
 from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
+from .pages.basket_page import BasketPage
+import pytest
+link = "http://selenium1py.pythonanywhere.com/"
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
-    login_page = LoginPage(browser, browser.current_url)          # проинициализировать новый объект Page # передаем ему тот же самый объект драйвера для работы с браузером, а в качестве url передаем текущий адрес
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+    def test_guest_can_go_to_login_page(self, browser):
+        """Тест перехода на страницу логина"""
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
 
-
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        """Тест наличия кнопки логирования/регистрации"""
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
 def test_url_address_matching(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = LoginPage(browser, link)                          # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                                              # открываем страницу
+    """Тест корректности url адреса"""
+    page = LoginPage(browser, link)
+    page.open()
+    page.go_to_login_page()
     page.should_be_login_url()
+
+
+def test_availability_of_a_login_form(browser):
+    """Тест наличия формы логирования"""
+    page = LoginPage(browser, link)
+    page.open()
+    page.go_to_login_page()
     page.should_be_login_form()
+
+
+def test_availability_of_a_register_form(browser):
+    """Тест наличия формы регистрации"""
+    page = LoginPage(browser, link)
+    page.open()
+    page.go_to_login_page()
     page.should_be_register_form()
-    """Переделать в три теста"""
-    """Записать в тетрадь обработку alert в go_to_login_page, если он, например, появляется при нажатии на кнопку логина"""
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    """Тест перехода в корзину и проверки, что она пуста"""
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page.basket_is_empty()
