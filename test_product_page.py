e7517ea6a00ce07270276700524bd17b0c473b09
EvgeniyMarin <evgeniymarin7@mail.ru>
2023-11-22 11:46:48 +0300

    improving code readability 1.1

--- test_product_page.py
+++ test_product_page.py
@@ -1,14 +1,110 @@
 from .pages.product_page import ProductPage
-import time
+from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
+import pytest
+import faker
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.need_review
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    """Тест добавления товара в корзину"""
     page = ProductPage(browser, link)
     page.open()
     page.adding_to_basket()
     page.solve_quiz_and_get_code()
     page.checking_the_addition_to_basket()
     page.checking_price_compliance()
-    time.sleep(10)
 
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
+
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    """Негативный тест на проверку отсутствия элемента"""
+    page = ProductPage(browser, link)
+    page.open()
+    page.adding_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    """Тест на проверку отсутствия элемента"""
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    """Сообщение о добавлении в корзину исчезло со временем"""
+    page = ProductPage(browser, link)
+    page.open()
+    page.adding_to_basket()
+    page.solve_quiz_and_get_code()
+    page.message_should_disappear()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    """Тест наличия кнопки логина/регистрации"""
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    """Тест перехода на страницу логирования/регистрации"""
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    """Тест перехода в корзину и проверки, что она пуста"""
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page.basket_is_empty()
+
+
+@pytest.mark.User_Basket
+class TestUserAddToBasketFromProductPage:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        f = faker.Faker()
+        email = f.email()
+        password = "D11172023"
+        print(f"Созданная почта: {email}, пароль: {password}")
+        page = LoginPage(browser, link)
+        page.open()
+        page.should_be_login_url()
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        """Тест на проверку отсутствия элемента"""
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
+        """Тест добавления товара в корзину"""
+        page = ProductPage(browser, link)
+        page.open()
+        page.adding_to_basket()
+        page.checking_the_addition_to_basket()
+        page.checking_price_compliance()
